6adcd5d9f3273f49bf00ae28ce5035f3c499b1d7
Jose David <jose_david_51@hotmail.com>
2021-09-30 08:17:57 +0200

    Primer método completado, cambios en los estilos y cambios en el script de la BBDD.

--- BD_videoclub_insercciones.sql
+++ BD_videoclub_insercciones.sql
@@ -1,24 +1,24 @@
 use videoclub;
 
 INSERT INTO peliculas VALUES
-(1,'Gladiator','Peplum y Accion', 'UK', 2000, 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSBPZVJZHi4yO6Ykm576nF6pfeqhYcb8jitE9vorT6k0rBDMPLG'),
-(2,'Marte','Ciencia Ficcion', 'EEUU', 2015, 'https://es.web.img3.acsta.net/pictures/15/08/27/13/26/081921.jpg'),
-(3,'El Codigo Da Vinci','Misterio y Suspense', 'EEUU', 2006, 'https://es.web.img3.acsta.net/pictures/14/03/25/13/38/299195.jpg'),
-(4,'300','Accion y Cine Epico', 'EEUU', 2007, 'https://es.web.img3.acsta.net/medias/nmedia/18/67/71/17/20027466.jpg'),
-(5,'El Medico','Aventuras', 'Germany', 2013, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREF2tx0QdcHx9Fc6VYXrGGZc_dR8IdIxFbdCIWIJot4OEfeAIc'),
-(6,'John Carter','Ciencia Ficcion', 'EEUU', 2012, 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRyqjINEgNdxZ4jotb5eP_VT2oiTQK9VtdGFlrPe6SPsJkW0E-c'),
-(7,'La Cumbre Escarlata','Suspenso', 'Mexico', 2015, 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRW8PFbkubM-9b3cAXxt37d4kNxHSTt0tlhjbyemS9sPFT9JmOf'),
-(8,'Regresion','Suspenso y Misterio', 'Spain', 2015, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW8xs0ryLszoXq0LqKk2e4-dHUlebarlhbFnNvUdo-qLtrSWkN');
+(1,'Gladiator','Peplum y Accion', 'UK', 2000, '1_gladiator.jfif'),
+(2,'Marte','Ciencia Ficcion', 'EEUU', 2015, '2_marte.jpg'),
+(3,'El Codigo Da Vinci','Misterio y Suspense', 'EEUU', 2006, '3_Codigo_Da_Vinci.jpg'),
+(4,'300','Accion y Cine Epico', 'EEUU', 2007, '4_300.jpg'),
+(5,'El Medico','Aventuras', 'Germany', 2013, '5_El_Medico.jfif'),
+(6,'John Carter','Ciencia Ficcion', 'EEUU', 2012, '6_John_Carter.jfif'),
+(7,'La Cumbre Escarlata','Suspenso', 'Mexico', 2015, '7_La_Cumbre_Escarlata.jfif'),
+(8,'Regresion','Suspenso y Misterio', 'Spain', 2015, '8_Regresion.jfif');
 
 INSERT INTO personas VALUES
-(1,'Russell','Crowe', 'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcR9BTx63RrKZ1RdF7wCncuUNuolDUOs2k4hCr0iyetnOK0DNkpX37iE4LNDKVJJ'),
-(2,'Matt','Damon', 'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcR8QBCTC4RhzZMcahergvqlVG6FQWpKD_IHvYf3Y_t2mN6FA2ZA6L117u2DoCOT'),
-(3,'Tom','Hanks', 'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQQEm8_HDtjPqgJK8zoKWC-nAgvs89gTEQrKyHcLilSEaqO-bBES20GJphMKaID'),
-(4,'Gerard','Butler', 'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRVXuQFBfEa4UfWvq0xuFF7-zDO9IKU-_6GjEsHdDZGiFEPvmNnQzL57xjgBwzh'),
-(5,'Tom','Payne', 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSn4nZRLNKukLUaAG3ESCsIAGIjpsVtgESYfoRVZ9vCW-oy2jzYZ73VwOM28bvf'),
-(6,'Taylor','Kitsch', 'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQeJdNM3YWS7OZm21GulC66oi4fCQDTCrPDtgW_jyGSV6TcwuNmOrmqVtIciX_8'),
-(7,'Mia','Wasikowska', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS9MTxwhgQQYxqgAKNkWkNZwHamFhXSV_BD0QGUYDFBQK3TaOFi'),
-(8,'Emma','Watson', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTdR8EBtqfxKIj8-vazEROe6JEkpca-oJMaesCzmF3tni5KbwaW');
+(1,'Russell','Crowe', '1_Russel_Crowe.jfif'),
+(2,'Matt','Damon', '2_Matt_Damon.jfif'),
+(3,'Tom','Hanks', '3_Tom_Hanks.jfif'),
+(4,'Gerard','Butler', '4_Gerard_Butler.jfif'),
+(5,'Tom','Payne', '5_Tom_Payne.jfif'),
+(6,'Taylor','Kitsch', '6_Taylor_Kitsch.jfif'),
+(7,'Mia','Wasikowska', '7_Mia_Wasikowska.jfif'),
+(8,'Emma','Watson', '8_Emma_Watson.jfif');
 
 INSERT INTO actuan VALUES
 (1,1),
--- videoclub.php
+++ videoclub.php
@@ -10,7 +10,7 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/inicio.css" />
+    <link rel="stylesheet" type="text/css" href="css/inicio.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -27,6 +27,11 @@ if (isset($_REQUEST["action"])) {
     $action = "mostrarListaPeliculas";  // Acción por defecto
 }
 
+//VARIABLES
+$ruta_Imagen_Pelicula = "http://localhost/Videoclub_PHP/images/films/";
+$ruta_Imagen_Persona = "http://localhost/Videoclub_PHP/images/actors/";
+$estilo = "style=width:50px;height:70px;";
+
 
 // CONTROL DE FLUJO PRINCIPAL
     // El programa saltará a la sección del switch indicada por la variable "action"
@@ -35,12 +40,14 @@ if (isset($_REQUEST["action"])) {
         // --------------------------------- MOSTRAR LISTA DE LIBROS ----------------------------------------
 
         case "mostrarListaPeliculas":
-            echo "<h1>Películas</h1>";
+            echo "<h1>Películas y Actores</h1>";
 
             // Buscamos todas las películas del videoclub
             if ($result = $db->query("SELECT * 
-                                        FROM peliculas
+                                        FROM peliculas inner join actuan on peliculas.idPelicula = actuan.idPelicula 
+                                        inner join personas on actuan.idPersona = personas.idPersona
                                         ORDER BY peliculas.idPelicula")) {
+                                            //inner join personas on peliculas.id_Pelicula = personas.idPersona
 
                 // La consulta se ha ejecutado con éxito. Vamos a ver si contiene registros
                 if ($result->num_rows != 0) {
@@ -56,12 +63,19 @@ if (isset($_REQUEST["action"])) {
                     // Ahora, la tabla con los datos de los libros
                     echo "<table border ='1'>";
                     echo "<tr>
-                        <th>ID</th>
+                        <th>ID_Película</th>
                         <th>Título</th>
                         <th>Género</th>
                         <th>País</th>
                         <th>Año</th>
                         <th>Cartel</th>
+                        
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Fotografia</th>
+
+                        <th>Update</th>
+                        <th>Delete</th>
                     </tr>";
                     while ($fila = $result->fetch_object()) {
                         echo "<tr>";
@@ -70,9 +84,18 @@ if (isset($_REQUEST["action"])) {
                         echo "<td>" . $fila->genero . "</td>";
                         echo "<td>" . $fila->pais . "</td>";
                         echo "<td>" . $fila->anyo . "</td>";
-                        echo "<td>" . $fila->cartel . "</td>";
-                        //echo "<td><a href='videoclub.php?action=formularioModificarLibro&idPelicula=" . $fila->idPelicula . "'>Modificar</a></td>";
-                        //echo "<td><a href='videoclub.php?action=borrarLibro&idPelicula=" . $fila->idPelicula . "'>Borrar</a></td>";
+                        //echo "<td><img src='$ruta_Imagen" . $fila->cartel . "' style=width:40px;height:55px;></td>";
+                        echo "<td><img src='$ruta_Imagen_Pelicula" . $fila->cartel . "'$estilo></td>";
+                        
+                        
+                        //echo "<td>" . $fila->idPersona . "</td>";
+                        echo "<td>" . $fila->nombre . "</td>";
+                        echo "<td>" . $fila->apellidos . "</td>";
+                        //echo "<td>" . $fila->fotografia . "</td>";
+                        echo "<td><img src='$ruta_Imagen_Persona" . $fila->fotografia . "'$estilo></td>";
+                        
+                        echo "<td><a href='videoclub.php?action=formularioModificarLibro&idPelicula=" . $fila->idPelicula . "'>Modificar</a></td>";
+                        echo "<td><a href='videoclub.php?action=borrarLibro&idPelicula=" . $fila->idPelicula . "'>Borrar</a></td>";
                         echo "</tr>";
                     }
                     echo "</table>";
@@ -88,13 +111,129 @@ if (isset($_REQUEST["action"])) {
             break;
 
 
+            // --------------------------------- BORRAR PELÍCULAS ----------------------------------------
+
+        case "borrarPelicula":
+            echo "<h1>Borrar películas</h1>";
+
+            // Recuperamos el id del libro y lanzamos el DELETE contra la BD
+            $idPelicula = $_REQUEST["idPelicula"];
+            $db->query("DELETE FROM peliculas WHERE idPelicula = '$idPelicula'");
+
+            // Mostramos mensaje con el resultado de la operación
+            if ($db->affected_rows == 0) {
+                echo "Ha ocurrido un error al borrar el libro. Por favor, inténtelo de nuevo";
+            } else {
+                echo "Libro borrado con éxito";
+            }
+            echo "<p><a href='index.php'>Volver</a></p>";
+
+            break;
 
 
-        
+            // --------------------------------- BUSCAR PELICULAS ----------------------------------------
+
+        case "buscarPeliculas":
+            // Recuperamos el texto de búsqueda de la variable de formulario
+            $textoBusqueda = $_REQUEST["textoBusqueda"];
+            echo "<h1>Resultados de la búsqueda: \"$textoBusqueda\"</h1>";
+
+            // Buscamos los libros de la biblioteca que coincidan con el texto de búsqueda
+            if ($result = $db->query("SELECT * FROM libros
+					INNER JOIN escriben ON libros.idLibro = escriben.idLibro
+					INNER JOIN personas ON escriben.idPersona = personas.idPersona
+					WHERE libros.titulo LIKE '%$textoBusqueda%'
+					OR libros.genero LIKE '%$textoBusqueda%'
+					OR personas.nombre LIKE '%$textoBusqueda%'
+					OR personas.apellido LIKE '%$textoBusqueda%'
+					ORDER BY libros.titulo")) {
+
+                // La consulta se ha ejecutado con éxito. Vamos a ver si contiene registros
+                if ($result->num_rows != 0) {
+                    // La consulta ha devuelto registros: vamos a mostrarlos
+                    // Primero, el formulario de búsqueda
+                    echo "<form action='index.php'>
+								<input type='hidden' name='action' value='buscarLibros'>
+                            	<input type='text' name='textoBusqueda'>
+								<input type='submit' value='Buscar'>
+                          </form><br>";
+                    // Después, la tabla con los datos
+                    echo "<table border ='1'>";
+                    while ($fila = $result->fetch_object()) {
+                        echo "<tr>";
+                        echo "<td>" . $fila->titulo . "</td>";
+                        echo "<td>" . $fila->genero . "</td>";
+                        echo "<td>" . $fila->numPaginas . "</td>";
+                        echo "<td>" . $fila->nombre . "</td>";
+                        echo "<td>" . $fila->apellido . "</td>";
+                        echo "<td><a href='index.php?action=formularioModificarLibro&idLibro=" . $fila->idLibro . "'>Modificar</a></td>";
+                        echo "<td><a href='index.php?action=borrarLibro&idLibro=" . $fila->idLibro . "'>Borrar</a></td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    // La consulta no contiene registros
+                    echo "No se encontraron datos";
+                }
+            } else {
+                // La consulta ha fallado
+                echo "Error al tratar de recuperar los datos de la base de datos. Por favor, inténtelo más tarde";
+            }
+            echo "<p><a href='index.php?action=formularioInsertarLibros'>Nuevo</a></p>";
+            echo "<p><a href='index.php'>Volver</a></p>";
+            break;
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // --------------------------------- ACTION NO ENCONTRADA ----------------------------------------
+
+        default:
+        echo "<h1>Error 404: página no encontrada</h1>";
+        echo "<a href='index.php'>Volver</a>";
+        break;
     } // switch
 
 
